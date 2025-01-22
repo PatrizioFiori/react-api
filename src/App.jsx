ed20de68d3ae9cc090c81738411d58c445b68c1f
PatrizioFiori <patrizio.fiori1995@gmail.com>
2025-01-22 12:47:47 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Main from './assets/components/Main'
+import Footer from './assets/components/Footer'
+import Header from './assets/components/Header'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
