be8b83d3ae207d5e84bc83cd126508f2c1ce163f
PatrizioFiori <patrizio.fiori1995@gmail.com>
2025-01-22 17:03:46 +0100

    wip (Viene richiamata la API e stampato in pagina il risultato)

--- src/assets/components/Posts.jsx
+++ src/assets/components/Posts.jsx
@@ -1,20 +1,41 @@
 import PostsCard from "./PostsCard"
+import axios from "axios"
+import { useState, useEffect } from "react"
 
 const Posts = () => {
+
+  const baseApiUrl = "http://localhost:3000"
+  const [posts, setPosts] = useState([])
+
+  const fetchPosts = () => {
+    axios.get(`${baseApiUrl}/posts`)
+      .then(res => {
+        setPosts(res.data)
+        console.log(JSON.stringify(posts, null, 2));
+      })
+      .catch(error => {
+        console.log("errore durante il caricamento", error);
+      })
+  }
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
   return (
-    <div>
-      <div className="container my-5">
-        <h1 className="text-center mb-5">
-          Gestione posts
-        </h1>
-        <div className="row">
-          <PostsCard />
-          <PostsCard />
-          <PostsCard />
-          <PostsCard />
+    <div className="container my-5">
+      <h1 className="text-center mb-5">
+        Gestione posts
+      </h1>
+      <div className="row bg-light">
+        {posts.map(post => {
+          return (
+            <PostsCard key={post.id} post={post} />
+          )
+        })}
 
 
-        </div>
+
       </div>
     </div>
   )
--- src/assets/components/PostsCard.jsx
+++ src/assets/components/PostsCard.jsx
@@ -1,15 +1,17 @@
-const PostsCard = () => {
+const PostsCard = (props) => {
+    const { image, title, tags, content, id } = props.post
     return (
         <div className="col-12 col-md-4 mb-5">
+
             <div className="card">
-                <img src="https://placehold.co/250" alt="img" />
+                <img src={image} alt={title} />
                 <div className="card-body">
-                    <h5 className="card-title">Titolo post</h5>
+                    <h5 className="card-title">{title}</h5>
                     <p className="card-text">
-                        Scheda: <strong>Elemento1, elemento2, elemento3</strong>
+                        Scheda: <strong>{[tags.join(', ')]}</strong>
                     </p>
                     <div className="card-text mb-3">
-                        Autore
+                        <strong>Content</strong>: {content}
                     </div>
                     <div className="mt-1 btn btn-danger">
                         <i class="fa-solid fa-trash"></i> Elimina
